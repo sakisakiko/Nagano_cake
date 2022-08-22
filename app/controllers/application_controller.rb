a527f43bfee395999750e524aa34a855ff364fd5
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-22 14:20:01 +0000

    add routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+ 
+    def after_sign_in_path_for(resource)
+      # 後で修正
+      about_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,41 @@
 Rails.application.routes.draw do
   
+   root to:"public/homes#top"
+   get '/about'=>'public/homes#about',as: 'about'
+   resources:items,only:[:index,:show]
+   
+   resources:cart_items,only:[:create,:index,:update,:destroy]
+   delete '/cart_items/destroy_all'=>'public/cart_items#destroy_all',as: 'destroy_all'
+   
+   resources:orders,only:[:create,:new,:index,:show]
+   post '/orders/confirm'=>'public/orders#confirm',as: 'confirm'
+   get '/orders/complete'=>'public/orders#complete',as: 'complete'
+   
+   
+   resources:addresses,only:[:index,:edit,:create,:update,:destroy]
+   
+  
+   get '/customers/my_page'=>'public/customers#show',as: 'my_page'
+   get '/customers/information/edit'=>'public/customers#edit',as: 'information_edit'
+   patch '/customers/information'=>'public/customers#update',as: 'information'
+   get '/customers/unsubscribe'=>'public/customers#unsubscribe',as: 'unsubscribe'
+   patch '/customers/withdraw'=>'public/customers#withdraw',as: 'withdraw'
+   
+   
+  
+  
   namespace :admin do
-    get 'orders/show'
+    get '/admin'=>'admin/homes#top', as: 'admin_top'
+    resources:customers,only:[:index,:show,:edit,:update]
+    resources:genres,only:[:create,:index,:edit,:update]
+    resources:items,only:[:new,:create,:index,:show,:edit,:update]
+    
+    resources:orders,only:[:show,:update] do
+     resources:order_details,only:[:update]
+    end
+    
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'items/ingex'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
+ 
   #顧客用
   #URL/customers/sign_in...
   devise_for :customers,skip: [:passwords],controllers: {
