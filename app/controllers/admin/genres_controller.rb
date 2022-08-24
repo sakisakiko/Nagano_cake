b94a0ca65b4bbe95f2ca4be53de1c28fa6fc8236
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-24 08:53:15 +0000

    fix wrong code on genres_controller,genres/index.html,edit.html

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,27 +1,32 @@
 class Admin::GenresController < ApplicationController
   
-  
-  def create 
-    @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to edit_admin_genre_path(genre_params[:id])
-  end
-  
   def index
     @genre=Genre.new
     @genres=Genre.all
   end
+  
+  def create 
+    @genre=Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+  
 
   def edit
     @genre=Genre.find(params[:id])
   end
   
   def update 
-    @genre=Genre.find(paramd[:id])
-    @genre.update(genre.params)
+    @genre=Genre.find(params[:id])
+    @genre.update(genre_params)
     redirect_to admin_genres_path
   end
   
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 
 
 end
