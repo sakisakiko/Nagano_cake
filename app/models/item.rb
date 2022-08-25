4b5c16862dee9b743978beb25aceff4f3cdaeee5
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-25 10:48:52 +0000

    [Fix]wrong on admin/items/new.html,index,html [Add]code show.html.erb

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,24 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items,dependent: :destroy
   has_many :order_details,dependent: :destroy
-  
+ 
+  #販売ステータスを確認するバリデーション
  validates :is_active, inclusion: { in: [true, false] }
  
+  # 消費税を加えた商品価格
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+  
+
+  
+    def  get_image(width,height)
+     unless image.attached?
+       file_path=Rails.root.join('app/assets/images/l_e_others_501.jpeg')
+       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     end
+       image.variant(resize_to_limit: [width, height]).processed
+    end
+  
+   
 end
