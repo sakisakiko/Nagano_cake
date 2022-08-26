45e039f359c2dfb5d797ead442229266c3063881
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-26 09:57:08 +0000

    add code on admin/customers/index.html.erb

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,30 @@
 class Admin::CustomersController < ApplicationController
+  
   def index
+     @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer=Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer=Customer.find(params[:id])
+  end
+  
+  def update
+    @customer=Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to  admin_customer_path(customer.id)
+  end
+  
+  
+  private
+
+  def item_params
+    params.require(:customer).permit(:email,:last_name,:first_name,
+    :last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:is_deleted)
   end
 
-  def edit
-  end
+
 end
