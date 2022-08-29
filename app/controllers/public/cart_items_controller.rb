004b71b54175d954555003a4eb3756d3a53de157
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-29 03:46:47 +0000

    add action'uodate' on cart_items_controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,12 +7,17 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item=CartItem.new(cart_item_params)
     @cart_item.customer_id=current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    
+    if cuurent_customer.cart_item.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item.update(cart_item_params)
+      redirect_to cart_item_path(@cart_item.id)
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
-  
-  
-  
+
+
   private
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :amount)
