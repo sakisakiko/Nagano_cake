9106bd3df8e042b6034b4eb0e2768b6d1bfa52aa
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-01 13:03:21 +0000

    add confirm-action on public/orders_controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,7 +7,24 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order=Order.new(order_params)
+    @orders=Order.all
+    @order.select_address=Order.find(params[:order][:select_address])
+    if @order.select_address== "1"
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
+    elsif @order.select_address== "2"
+     @address = Address.find(params[:order][:address_id])
+    else @order.select_address== "3"
+    @order=Order.new(order_params)
+    end
+
   end
+
+
+
+
+
 
 
   def create
@@ -21,12 +38,14 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-end
 
  private
   def order_params
-   params.require(:order).permit(:customer_id,:shipping_cost,:payment_price,:name,:postal_code,:address,:payment_method,:status)
+   params.require(:order).permit(:name,:postal_code,:address,:payment_method)
   end
+
+
+end
 
 
 
