65ca154bcf2c41d2a8a58d45972cd75f96898dfe
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-02 09:13:08 +0000

    add about_page

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,15 +8,14 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order=Order.new(order_params)
     @orders=Order.all
-    @order.select_address=Order.find(params[:order][:select_address])
-    if @order.select_address== "1"
+    @order=Order.find(params[:order][:select_address])
+    if @order== "1"
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @order.name = current_customer.last_name + current_customer.first_name
-    elsif @order.select_address== "2"
+    elsif @order== "2"
      @address = Address.find(params[:order][:address_id])
-    else @order.select_address== "3"
-    @order=Order.new(order_params)
+    else @order== "3"
     end
 
   end
