391dec0541692df63e2ac2b4abc2766753568e0e
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-06 07:34:37 +0000

    add code view/public/orders/show.html.erb and customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,17 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer=current_customer
   end
 
   def edit
+    @customer=current_customer
   end
+
+  def update
+    @customer=current_customer
+    @customer.update(current_customer)
+    redirect_to my_page_path
+  end
+
+
 end
