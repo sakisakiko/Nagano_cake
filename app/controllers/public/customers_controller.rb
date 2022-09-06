68d074c71d2499b7dae0def23a0b3927601777d3
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-06 08:32:13 +0000

    [add]code on view/public/customers/edit.html/erb [add] strong-parameter　on customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,9 +9,16 @@ class Public::CustomersController < ApplicationController
 
   def update
     @customer=current_customer
-    @customer.update(current_customer)
+    @customer.update(customer_params)
     redirect_to my_page_path
   end
+
+  private
+  def customer_params
+   params.require(:customer).permit(:email,:last_name,:first_name,
+   :last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number)
+  end
+
 
 
 end
