12eebffc230627782a5dfa2e0163de768042e4d8
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-07 10:27:37 +0000

    [fix] wrong code on confirm.html.erb

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,14 +10,33 @@ class Public::OrdersController < ApplicationController
     @orders=current_customer.orders
     @cart_items=current_customer.cart_items
     @order.shipping_cost= 800
-    @total_price=0
+    @order.payment_price=0
 
+    if params[:order][:select_address] == "1"   # 自身の住所のとき
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == "2" # 登録済み住所のとき
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else  params[:order][:select_address] == "3" # 新しい住所のとき
+    end
   end
 
-  def complete
-  end
 
   def create
+    @order=Order.new(order_params)
+    @order.customer=current_customer
+    @order.save
+    @cart_items=current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to complete_path
+  end
+  
+  
+  def complete
   end
 
 
