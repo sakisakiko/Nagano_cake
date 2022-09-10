41645352bebcd786bada6e941e08adf3ac77f24f
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-10 07:35:39 +0000

    add code admin/orders/show.html.erb

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order=Order.find(patams[:id])
+    @order_details=OrderDetail.find_by(order_id)
   end
   
   
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,6 +2,9 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
   
+  enum status: {　untouched: 0,  preparation: 1, making: 2,completed: 3 }
+
+  
     ## 小計を求めるメソッド
   def subtotal
     price * amount
