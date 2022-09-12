d8ab051e1e7e0ba4be0332055f331009ff2246b5
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-12 07:14:49 +0000

    add code on public/unsbscribe.html.erb and on customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,6 +13,18 @@ class Public::CustomersController < ApplicationController
     redirect_to my_page_path
   end
 
+  def unsubscribe
+    @customer=current_customer
+  end
+
+
+  def withdraw
+    @customer=current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
   private
   def customer_params
    params.require(:customer).permit(:email,:last_name,:first_name,
