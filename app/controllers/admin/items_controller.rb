81d875bbd286d903a7ffb4e342d79e8c7ab6e0b5
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-17 11:07:16 +0000

    add code on admin/cart_items/index.html.erb

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
   def new
     @item=Item.new
   end
-  
+
   def create
     @item=Item.new(item_params)
     @item.save
@@ -21,18 +21,18 @@ class Admin::ItemsController < ApplicationController
   def edit
     @item=Item.find(params[:id])
   end
-  
+
   def update
     @item=Item.find(params[:id])
     @item.update(item_params)
     redirect_to admin_items_path(@item.id)
   end
-  
+
   private
 
   def item_params
     params.require(:item).permit(:image,:genre_id,:name,:introduction,:price,:is_active)
   end
-  
+
 
 end
