3a630ee680edd1981d40d4a525d548f364ade4ce
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-17 13:15:52 +0000

    add code on public/items/show.html.erb

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,8 +3,9 @@ class Admin::OrderDetailsController < ApplicationController
   @order=Order.find(params[:order_id])
   @order_detail=@order.order_details.find(params[:id])
   @order_detail.update(order_detail_params)
-  redirect_to admin_order_path(@order.id)
+     redirect_to admin_order_path(@order.id)
   end
+
 
   private
   def order_detail_params
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,17 +3,28 @@ class Admin::OrdersController < ApplicationController
     @order=Order.find(params[:id])
     @order.shipping_cost=800
   end
-  
+
   def update
-    @order=Order.find(params[:id])
+     @order=Order.find(params[:id])
+
+  if @order.update(status: 1)
+     @order.order_details.update_all(status: 1)
+     redirect_to admin_order_path(@order.id)
+  elsif@order.order_details.update(status: 2)
+       @order.update(status: 2)
+       redirect_to admin_order_path(@order.id)
+  elsif@order.order_details.update_all(status: 3)
+       @order.update(status: 4)
+       redirect_to admin_or
+  else
     @order.update(order_params)
-    redirect_to admin_order_path(@order.id)
+   end
   end
-  
-  
+
+
 
   private
-   
+
    def order_params
      params.require(:order).permit(:customer_id,:shipping_cost,:payment_price,
      :name,:postal_code,:address,:payment_method,:status)
