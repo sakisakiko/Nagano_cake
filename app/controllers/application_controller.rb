e6144db5e5273f7effc565e0871be7a7824889e6
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-17 09:51:56 +0000

    add code on application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,34 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+
+def after_sign_in_path_for(resource)
+  case resource
+  when Admin
+    admin_top_path
+  when Customer
+    root_path
+  end
+end
+
+def after_sign_up_path_for(resource)
+  case resource
+  when Customer
+    my_page_path
+  end
+end
+
+
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+  end
 
 
   protected
