df85a1a19f05c92fa0c5767346921c484e15cc9b
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-18 12:21:35 +0000

    add code_for_validate on public/address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,23 @@
 class Address < ApplicationRecord
+  MAX_POSTS_COUNT = 3
+  # 登録したい件数の制限数
+
   belongs_to :customer
-  
+
+  validate :addresses_count_must_be_within_limit
+  # 制限のためのバリデーション
+
   def address_display
-  '〒' + postal_code + ' ' + address + ' ' + name  
+  '〒' + postal_code + ' ' + address + ' ' + name
   end
-  
+
+
+   private
+
+    def addresses_count_must_be_within_limit
+      errors.add(:base, "addresses count limit: #{MAX_POSTS_COUNT}") if customer.addresses.count >= MAX_POSTS_COUNT
+    end
+    # 配送先の登録数が制限数を超えた時、エラーを返す
+
+
 end
