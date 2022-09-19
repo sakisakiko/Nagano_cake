4ecf1ef21dc55ba7f1541bb2a555047525ea077f
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-19 13:50:59 +0000

    fix association on genre.rb

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,31 +1,31 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
     @genre=Genre.new
     @genres=Genre.all
   end
-  
-  def create 
+
+  def create
     @genre=Genre.new(genre_params)
     @genre.save
     redirect_to admin_genres_path
   end
-  
+
 
   def edit
     @genre=Genre.find(params[:id])
   end
-  
-  def update 
+
+  def update
     @genre=Genre.find(params[:id])
     @genre.update(genre_params)
     redirect_to admin_genres_path
   end
-  
+
   private
 
   def genre_params
-    params.require(:genre).permit(:name)
+    params.require(:genre).permit(:name,:id)
   end
 
 
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,3 @@
 class Genre < ApplicationRecord
-  has_many :genres,dependent: :destroy
+  has_many :items,dependent: :destroy
 end
