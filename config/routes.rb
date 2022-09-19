7b1809c124192f49df498ff4b561858f0d56b725
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-19 06:14:49 +0000

    delete resources:orders,only:[:destroy]

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
        post '/orders/confirm'=>'orders#confirm',as: 'confirm'
        get '/orders/complete'=>'orders#complete',as: 'complete'
-       resources:orders,only:[:create,:new,:index,:show,:destroy] # destroy 不要　あとで削除
+       resources:orders,only:[:create,:new,:index,:show] 
    
 
 
