c7e40f12fc31127d3b0b9b722a6cc894679c9c21
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-09-19 06:03:49 +0000

    fix wrong_code on admin/top.html.erb

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,6 +2,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+
   enum status: {  untouched: 0,  preparation: 1, making: 2, completed: 3 }
  # untouched: "着手不可" preparation: "製作待ち"  making: "製作中" completed: "製作完了"
 
@@ -10,5 +11,9 @@ class OrderDetail < ApplicationRecord
   def subtotal
     price * amount
   end
+  
 
 end
+
+
+
